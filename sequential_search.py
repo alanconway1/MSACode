77f9e3fceb18390c10b048c8068a415e756df33d
alanconway1 <136748774+alanconway1@users.noreply.github.com>
2023-06-21 14:39:54 -0500

    Add files via upload

---
+++ sequential_search.py
@@ -0,0 +1,20 @@
+import random 
+
+def sequential_search(number_list, target_value):
+    for index in range(len(number_list)): 
+        #determine if the number at the current index is equal to target 
+        #if return the index and end the loop 
+        if target_value == number_list[index]:
+            return index
+        
+    return -1
+
+def main():
+    #creates list of 100 numbers from 1 to 100
+    number_list = random.sample(range(1,101), 100)
+    number_list.sort()
+
+    found_index = sequential_search(number_list, 105)
+
+    print(found_index)
+main()
